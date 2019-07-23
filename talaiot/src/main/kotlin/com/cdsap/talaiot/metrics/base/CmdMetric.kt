33c33785a640b3bac623b1d0c27251c1931a63be
Anton Malinskiy <anton@malinskiy.com>
2019-07-23 20:47:32 +1000

    More docs

--- talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/base/CmdMetric.kt
+++ talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/base/CmdMetric.kt
@@ -2,10 +2,14 @@ package com.cdsap.talaiot.metrics.base
 
 import com.cdsap.talaiot.metrics.SimpleMetric
 import com.cdsap.talaiot.entities.ExecutionReport
+import org.gradle.BuildResult
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.IllegalStateException
 
+/**
+ * [Metric] that operates on some command line action output
+ */
 open class CmdMetric(val cmd: String, assigner: (ExecutionReport, String) -> Unit): SimpleMetric<String>(
     provider = {
         val runtime = Runtime.getRuntime()
